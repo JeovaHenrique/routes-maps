08781a899710d013eb295e52f83455804524120c
Jeova Henrique <jeovahenrique92@gmail.com>
2022-02-19 20:22:49 -0300

    Projeto finalizado

--- components/Maps.js
+++ components/Maps.js
@@ -1,46 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useCallback} from 'react'
 import MapView, {Marker} from 'react-native-maps'
 import { StyleSheet, View, Dimensions } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
 
 export default function Maps({ navigation }) {
     
     const [map, setMap] = useState([])
-    const [latitude, setLatitude] = useState(-5.843387691883379)
-    const [longitude, setLongitude] = useState(-35.19941719489336)
         
-        useEffect(() => {
-            async function getData() {
-                
-                const token = 'vv7oTsHdw0X9g5e7QbniP58j3iJY4h6AoOSxMIw2X8xjokSHjF'
-                
-                const headerOptions = {
-                    method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }   
-                const response = await fetch('https://mobile.ect.ufrn.br:3003/markers', headerOptions)
-                const map = await response.json()
-                setMap(map)
-            }
-            getData()
-        },[])
-
+        useFocusEffect(
+            useCallback(() => {
+                async function getData() {
+                    
+                    const token = 'vv7oTsHdw0X9g5e7QbniP58j3iJY4h6AoOSxMIw2X8xjokSHjF'
+                    
+                    const headerOptions = {
+                        method: 'GET',
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }   
+                    const response = await fetch('https://mobile.ect.ufrn.br:3003/markers', headerOptions)
+                    const map = await response.json()
+                    setMap(map)
+                }
+                getData()
+            },[])
+        )
 
     return (
         <View style={styles.container}>
             
             
-            <MapView style={styles.map}
-            onPress={(e) => {
-                setLatitude(e.nativeEvent.coordinate.latitude)
-                setLongitude(e.nativeEvent.coordinate.longitude)
-            }}>
-            
+            <MapView style={styles.map}>     
             <Marker
                 coordinate={{
-                        latitude,
-                        longitude,
+                        latitude: -5.843387691883379,
+                        longitude:-35.19941719489336,
                     }}
                 title="ECT"
                 description="Escola de Ciências e Tecnologia"
--- screens/AddMarker.js
+++ screens/AddMarker.js
@@ -1,109 +1,15 @@
-import React, { useState, useEffect} from "react"
+import React, { useState} from "react"
 import MapView, {Marker} from 'react-native-maps'
 import { StyleSheet, View, TouchableOpacity, StatusBar, Text, TextInput, Dimensions } from "react-native"
 
+import MarkerMaps from '../components/MarkerMaps'
+
 export default function AddMarker({navigation}) {
-
-    const [map, setMap] = useState([])
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [latitude, setLatitude] = useState(0)
-    const [longitude, setLongitude] = useState(0)
-
-    useEffect(() => {
-        async function getData() {
-            
-            const token = 'vv7oTsHdw0X9g5e7QbniP58j3iJY4h6AoOSxMIw2X8xjokSHjF'
-            
-            const headerOptions = {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }   
-            const response = await fetch('https://mobile.ect.ufrn.br:3003/markers', headerOptions)
-            const map = await response.json()
-            setMap(map)
-        }
-        getData()
-    },[])
-
-    async function Marcador() {
-
-        const token = 'vv7oTsHdw0X9g5e7QbniP58j3iJY4h6AoOSxMIw2X8xjokSHjF'
-
-        const headerOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-                latitude,
-                longitude,
-                title,
-                description,
-                }),
-        }
-            
-        const response = await fetch("https://mobile.ect.ufrn.br:3003/markers", headerOptions)
-        if (response.status === 200) {
-            alert('Adicionado com Sucesso!')
-        }
-    }
 
     return (
         <View style={styles.container}>
             <StatusBar style='auto'/>
-            <MapView style={styles.map}
-                onPress={(e) => {
-                setLatitude(e.nativeEvent.coordinate.latitude)
-                setLongitude(e.nativeEvent.coordinate.longitude)
-            }}>
-            
-                <Marker
-                    coordinate={{
-                        latitude,
-                        longitude,
-                        }}
-                    title={title}
-                    description={description}
-                >
-                </Marker>
-                {
-                 map.map((mark, id) => 
-                    <Marker 
-                        key={id}
-                        coordinate={{
-                            latitude: mark.latitude,
-                            longitude: mark.longitude,
-                        }}
-                        title={mark.title}
-                        description={mark.description}
-                    />
-                )
-            }
-            </MapView>
-            <View style={styles.addContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Titulo...'
-                    value={title}
-                    onChangeText={setTitle}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder= 'Descrição...'
-                    value={description}
-                    onChangeText={setDescription}  
-                />
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => Marcador()}>
-                <Text style={styles.text}>Marcar</Text>
-            </TouchableOpacity>
-            </View>
-        
+            <MarkerMaps/>
         </View>
     )
 }
@@ -112,46 +18,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-end',
-    },
-
-    addContainer: {
-        height: '40%',
-        backgroundColor: '#fff',
-        alignItems: 'center',   
-        justifyContent: 'center',
-        margin: 20,
-        borderColor: 'gray',
-        borderWidth: 1,
-        padding: 40,
-        borderRadius: 5,     
-    },
-
-    button: {
-        padding: 10,
-        width: 120,
-        backgroundColor: 'green',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 5,
-        marginTop: 20,
-        alignSelf: 'center',
-    },
-
-    text: {
-        fontSize: 15,    
-        color: 'white',
-        alignSelf: 'center',
-        
-    },
-
-    input: {
-        height: 40,
-        width: '90%',
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        marginTop: 5,
-        paddingLeft: 10,
     },
 
     map: {
